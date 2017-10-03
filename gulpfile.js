a21f3600db06f7ca3d577ea047da6f4fb2fe3d2b
betobaz <pedroalbertose007@gmail.com>
2017-10-02 19:28:42 -0500

    destrabando mv

--- gulpfile.js
+++ gulpfile.js
@@ -44,53 +44,47 @@ gulp.task("delete_directory", function* () {
 });
 
 gulp.task("extract_files",[
-  // "delete_directory"
+  "delete_directory"
 ], function*() {
   message = "Extrayendo restore files";
   spinner = ora(message).start();
-  // spinner.text = message;
 
   shell.cd(metadata.backup_dir);
   var tar_dir = instance_name+".sugarondemand.com."+metadata.sugar_version+metadata.sugar_flavor+".*";
-  // pv = shell.which('pv');
-  // if (pv) {
-  //   // console.log("Entrar para pv");
-  //   spinner.stop();
-  //   try {
-  //      files = []
-  //      shell.ls('*.gz').forEach(function (file) {
-  //         files.push(file);
-  //      });
-  //      if(files.length === 1){
-  //        promise = promiseFromChildProcess(
-  //          child_process.spawn('pv '+files[0]+' | tar xzf - -C . ', {
-  //            cwd: metadata.backup_dir,
-  //            stdio: 'inherit',
-  //            shell: true
-  //          })
-  //        );
-  //        yield promise;
-  //      }
-  //   } catch (err) {
-  //     console.error(`chdir: ${err}`);
-  //   }
-  // }
-  // else{
-  //   yield shell.exec("tar -zxvf "+ tar_dir + ".tar.gz");
-  // }
+  pv = shell.which('pv');
+  if (pv) {
+    // console.log("Entrar para pv");
+    spinner.stop();
+    try {
+       files = []
+       shell.ls('*.gz').forEach(function (file) {
+          files.push(file);
+       });
+       if(files.length === 1){
+         promise = promiseFromChildProcess(
+           child_process.spawn('pv '+files[0]+' | tar xzf - -C . ', {
+             cwd: metadata.backup_dir,
+             stdio: 'inherit',
+             shell: true
+           })
+         );
+         yield promise;
+       }
+    } catch (err) {
+      console.error(`chdir: ${err}`);
+    }
+  }
+  else{
+    yield shell.exec("tar -zxvf "+ tar_dir + ".tar.gz");
+  }
   spinner.succeed(message);
   spinner.stop();
 
   message = "Obteniendo carpeta de la instancia";
-  // console.log(shell.pwd());
-  // shell.ls('*').forEach(function (file) {
-  //    console.log(file);
-  // });
-  // console.log(metadata.backup_dir);
   var extracted_directory = "";
   promise = readdir(metadata.backup_dir).then(function(files){
     files.forEach(function (file) {
-      if(fs.lstatSync(file).isDirectory()){
+      if(file !=  '.DS_Store' && fs.lstatSync(file).isDirectory()){
         extracted_directory = file;
       }
     });
@@ -100,30 +94,26 @@ gulp.task("extract_files",[
 
   message = "Moviendo carpeta de la instancia";
   backup_dir = metadata.backup_dir + '/' + extracted_directory;
-  console.log(backup_dir);
-  promise = promiseFromChildProcess(
-    child_process.spawn("mv sugar" + metadata.sugar_version+metadata.sugar_flavor + " " + instance_dir, {
-      cwd: backup_dir,
-      stdio: 'inherit',
-      shell: true
-    });
-  );
-  ora.promise(promise, {text:message});
-  yield promise;
+  var command = "mv sugar" + metadata.sugar_version+metadata.sugar_flavor + " " + instance_dir;
+  var spawnMv = child_process.spawn(command, {
+    cwd: backup_dir,
+    stdio: 'inherit',
+    shell: true
+  });
+  promise2 = promiseFromChildProcess(spawnMv);
+  ora.promise(promise2, {text:message});
+  yield promise2;
 
-  // promise = shell.exec("mv sugar" + metadata.sugar_version+metadata.sugar_flavor + ".sql " + instance_dir);
-  // message = "Moviendo archivo SQL";
-  // ora.promise(promise, {text:message});
-  // yield promise;
-  // promise = promiseFromChildProcess(
-  //   child_process.spawn("mv sugar" + metadata.sugar_version+metadata.sugar_flavor + " " + instance_dir, {
-  //     cwd: metadata.backup_dir,
-  //     stdio: 'inherit',
-  //     shell: true
-  //   })
-  // );
-  // ora.promise(promise, {text:message});
-  // yield promise;
+  message = "Moviendo archivo SQL";
+  spawnMv = child_process.spawn("mv sugar" + metadata.sugar_version+metadata.sugar_flavor + ".sql " + instance_dir, {
+    cwd: metadata.backup_dir,
+    stdio: 'inherit',
+    shell: true
+  });
+
+  promise3 = promiseFromChildProcess(spawnMv);
+  ora.promise(promise3, {text:message});
+  yield promise3;
   return;
 });
 
